1c2be303d1116d2265dd3cb89a27dd6687ca6d80
vaishdotcom <vaishali.raja@student.uts.edu.au>
2022-09-16 15:23:41 +1000

    80% done

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -18,6 +18,21 @@ public class InputManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            tweener.AddTween(item.transform, item.transform.position, new Vector3(-2.0f, 0.5f, 0.0f), 1.5f);
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            tweener.AddTween(item.transform, item.transform.position, new Vector3(2.0f, 0.5f, 0.0f), 1.5f);
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            tweener.AddTween(item.transform, item.transform.position, new Vector3(0.0f, 0.5f, -2.0f), 0.5f);
+        }
+        else if (Input.GetKeyDown(KeyCode.W))
+        {
+            tweener.AddTween(item.transform, item.transform.position, new Vector3(0.0f, 0.5f, 2.0f), 0.5f);
+        }
     }
 }
--- Assets/Scripts/Tweener.cs
+++ Assets/Scripts/Tweener.cs
@@ -9,13 +9,25 @@ public class Tweener : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+      
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!(activeTween==null) && (double)Vector3.Distance(activeTween.Target.position, activeTween.EndPos) > 0.1)
+        {
+            float time = (Time.time - activeTween.StartTime) / activeTween.Duration;
+            activeTween.Target.position = Vector3.Lerp(activeTween.StartPos, activeTween.EndPos, time);
+        }
+        else
+        {
+            if(!(activeTween == null))
+            {
+                activeTween.Target.position = activeTween.EndPos;
+                activeTween = null;
+            }
+        }
     }
 
     public void AddTween(Transform targetObject, Vector3 startPos, Vector3 endPos, float duration)
